83b93512c5dc258b868dc0001d43f221799f46ea
Xpl0itU <samuelcarballogarcia8@gmail.com>
2023-01-08 01:27:47 +0100

    Remove dankvideos

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ for root, _, files in os.walk("assets"):
         for file in files:
             os.remove(os.path.join(root, file))
 
-subreddits = ["videomemes", "shitposting", "funnyvideos", "doodoofart", "LaughterWorld", "dankvideos"]
+subreddits = ["videomemes", "shitposting", "funnyvideos", "doodoofart", "LaughterWorld"]
 
 for subreddit in subreddits:
     reddit_fetch.scrape(subreddit)
